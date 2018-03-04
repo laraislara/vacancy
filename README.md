fdcc52e5867ccd16c4a148c04962a69b29bb7029
Lara Opaleva <larachemik@yandex.ru>
2018-03-04 18:32:26 +0300

    Added HH API

--- README.md
+++ README.md
@@ -1,35 +1,9 @@
-# HHmap
-React App for searching jobs on map
-# Описание
-  Создание React приложения по поиску работы на карте, используя Yandex Maps API и HeadHunter API
-# Установка
-  Склонируйте или загрузите репозиторий
-  ```Shell
-  $ git clone https://github.com/iliyaking46/hhmap.git
-  ```
-  Установите зависимости командой
-  ```shell
-    yarn install
-  ```
-  или
-  ```shell
-    npm install
-  ```
-  
-  Запустите `yarn` или `npm` с командой `start` для запуска dev-сервера
-  ```Shell
-  yarn start
-  ```
-  или
-  ```Shell
-  npm start
-  ```
-# Сборка
-  Запустите `yarn` или `npm` с командой `build` для сборки проекта (выходная директория `./build`)
-```Shell
-yarn build
-```
-  или
-```Shell
-npm run build
+#### HH API
+
+##### How to run the app:
+
+```bash
+git clone https://github.com/laraislara/vacancy.git && cd vacancy
+
+npm i && npm start
 ```
--- src/App.js
+++ src/App.js
@@ -1,96 +1,150 @@
-import React, { Component } from 'react';
-import MetroSelectBox from './components/MetroSelectBox';
-import TextBox from './components/TextBox';
-import Button from './components/Button';
-import JobsTable from './components/JobsTable';
+import React, { Component } from 'react'
+import './App.css'
+import Autocomplete from 'react-autocomplete'
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      metro: [], //тут хранятся все линии, а в них станции (stations)
-      data: [], //вакансии
-      metroStationId: '', //хранится конкретная станция (поставил кутузувскую, там вакансии есть)
-      text: 'react', //фраза для поиска
-      isLoad: false,
-      isLoadData: false,
-      error: '',
-    }
-  }
+const API_HH_URL = 'https://api.hh.ru/'
 
-  onFindJobs = () => {
-    this.setState({ isLoadData: false });
-    const perPage = 'per_page=10' //количество вакансий на странице
-    const metro = +this.state.metroStationId ? 'metro='+this.state.metroStationId : '';
-    const text = 'text='+this.state.text.split(' ').join('+');
-    const addUrl = [perPage, metro, text].join('&')
-    const baseUrl = 'https://api.hh.ru/vacancies?only_with_salary=true&'+addUrl;
-    fetch(`${baseUrl}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong ...');
-        }
-      })
-      .then(data => this.setState({ data, isLoadData: true }))
-      .catch(error => this.setState({ error }));
+const VacanciesTable = ({vacancies, isLoading}) => {
+  const vacanciesRow = vacancies.map(vacancy => (
+      <tr key={vacancy.id}>
+        <td><a href={vacancy.alternate_url}>{vacancy.name.slice(0,50)}</a></td>
+        <td>{vacancy.employer.name}</td>
+        <td>{new Date(vacancy.published_at).toLocaleString('ru', {year: 'numeric', month: 'numeric', day: 'numeric'})}</td>
+        <td>{vacancy.salary !== null && (`${vacancy.salary.from} ${vacancy.salary.currency}`)}</td>
+      </tr>
+    ),
+  )
+  return (
+    <table>
+      <thead>
+      <tr className='header'>
+        <td>Должность</td>
+        <td>Компания</td>
+        <td>Дата</td>
+        <td>Оклад</td>
+      </tr>
+      </thead>
+      <tbody>
+        {isLoading ? <tr><td colSpan="4">Loading...</td></tr> : vacanciesRow}
+      </tbody>
+    </table>
+  )
+}
+
+class App extends Component {
+  state = {
+    vacancies: [],
+    specializations: [],
+    metroStations: [],
+    vacancyValue: '',
+    metroValue: '',
+    isLoading: false,
   }
 
   componentDidMount() {
-    this.setState({ isLoad: false });
-    const metroUrl = 'https://api.hh.ru/metro/1'
-    fetch(`${metroUrl}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong ...');
-        }
-      })
-      .then(metro => this.setState({ metro: metro.lines, isLoad: true }))
-      .catch(error => this.setState({ error, isLoad: false }));
+    this.getSpecializationsData()
+    this.getMetroData()
   }
+
+  getSpecializationsData() {
+    fetch(`${API_HH_URL}specializations`)
+      .then(response => response.json())
+      .then(data => {
+        let labels = []
+        for (const el of data) {
+          for (const specialization of el.specializations) {
+            labels.push({
+              label: specialization.name,
+              specialization: el.name,
+              id: specialization.id,
+            })
+          }
+        }
+        this.setState({specializations: labels})
+      })
+      .catch(e => e)
+  }
+
+  getMetroData() {
+    fetch(`${API_HH_URL}metro/1`)
+      .then(response => response.json())
+      .then(data => {
+        let stations = []
+        for (const line of data.lines) {
+          for (const station of line.stations) {
+            stations.push({
+              label: station.name,
+              line: line.name,
+              id: station.id,
+            })
+          }
+        }
+        this.setState({metroStations: stations})
+      })
+      .catch(e => e)
+  }
+
+  handleSubmit = () => {
+    const vacancyValue = this.state.vacancyValue
+    const filteredMetro = this.state.metroStations.filter(el => el.label === this.state.metroValue)
+    const metroQuery = filteredMetro.length > 0
+      ? `&metro=${filteredMetro.shift().id}`
+      : ''
+    const vacanciesUrl = `${API_HH_URL}vacancies?area=1&text=${vacancyValue}${metroQuery}`
+    this.setState({isLoading: true})
+    fetch(vacanciesUrl)
+      .then(response => response.json())
+      .then(data => this.setState({vacancies: data.items, isLoading: false}))
+      .catch(e => e)
+}
 
   render() {
-    const { metro, data, metroStationId, text, isLoad, isLoadData } = this.state
-
-    if (!isLoad) return (<div>Loading...</div>) 
-   
-    return(
-    <div className="container mt-5">
-      <h1 className="text-center" >HH Map Jobs-Finder</h1>
-      <div className="row">
+    return (
+      <div className="container">
+        <h3>Ярмарка вакансий</h3>
+        <Autocomplete
+          getItemValue={(item) => item.label}
+          shouldItemRender={(item, value) => item.label.toLowerCase().includes(value.toLowerCase())}
+          items={this.state.specializations}
+          renderItem={(item, isHighlighted) =>
+            <div
+              key={item.id}
+              style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+              {item.label} [{item.specialization}]
+            </div>
+          }
+          value={this.state.vacancyValue}
+          onChange={(event, value) => this.setState({ vacancyValue: value })}
+          onSelect={value => this.setState({ vacancyValue: value })}
+          renderInput={(props) => <input id='inputVacancy' {...props} />}
+        />
+        <Autocomplete
+          getItemValue={(item) => item.label}
+          shouldItemRender={(item, value) => item.label.toLowerCase().includes(value.toLowerCase())}
+          items={this.state.metroStations}
+          renderItem={(item, isHighlighted) =>
+            <div
+              key={item.id}
+              style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+              {item.label} [{item.line}]
+            </div>
+          }
+          value={this.state.metroValue}
+          onChange={(event, value) => this.setState({ metroValue: value })}
+          onSelect={value => this.setState({ metroValue: value })}
+        />
+        <button onClick={this.handleSubmit} className='searchButton'>Поиск</button>
         <div>
-            <MetroSelectBox
-              value={metroStationId}
-              metro={metro}
-              onChange={id => this.setState({metroStationId: id})}
-            />
-          </div>
-          <div className="col">
-            <TextBox
-              onChange={(value) => this.setState({text: value})}
-              value={text}
-            />
-          </div>
-          <Button
-            onClick={this.onFindJobs}
-          >
-          Поиск
-          </Button>
+          {
+            this.state.vacancies.length > 0
+            && <VacanciesTable
+              vacancies={this.state.vacancies}
+              isLoading={this.state.isLoading}/>
+          }
+        </div>
       </div>
-      <div className="row justify-content-center">
-        { isLoadData ?
-          (
-            <JobsTable data={data}/>
-          ) : (
-            <div>Data is loading...</div>
-          )
-        }
-      </div>
-      
-    </div>)
-    
+    );
   }
 }
+
+export default App;
